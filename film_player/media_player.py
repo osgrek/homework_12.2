d01dc095fd9a56d6433c14a3c50dbf5085c0bb2c
grekalex <siralexgrek@gmail.com>
2023-07-26 21:03:56 +0300

    first 3 steps

---
+++ film_player/media_player.py
@@ -0,0 +1,17 @@
+class Player:
+    series_title: str
+    season_num: int
+    episode_title: str
+    description: str
+
+def volume_level():
+    pass
+
+def play():
+    pass
+
+def playback_speed():
+    pass
+
+def screen_mode():
+    pass
